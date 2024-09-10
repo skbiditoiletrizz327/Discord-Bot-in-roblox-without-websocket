e77c00d570c33a54e2bafa211a734e30c3957c11
skbiditoiletrizz327 <slomogurl37@gmail.com>
2024-09-10 08:57:18 -0700

    Update Client.lua

--- src/Roblox/Client.lua
+++ src/Roblox/Client.lua
@@ -1,11 +1,11 @@
-local CommandModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/skbiditoiletrizz327/Discord-Bot-in-roblox-without-websocket/main/src/Roblox/Module.lua"))()
+local CommandModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/skbiditoiletrizz327/Discord-Bot-in-roblox-without-websocket/main/src/Roblox/Module.lua"))() -- or your own url if you want
 
 CommandModule:AddCommand(".hi", function(arguments)
     print("Hello, world!")
 end)
 CommandModule:AddCommand(".say", function(arguments)
     local message = CommandModule:CompleteString(arguments)
-     game.ReplicatedStorage:FindFirstChild("DefaultChatSystemChatEvents").SayMessageRequest:FireServer(message, "All")
+     game.ReplicatedStorage:FindFirstChild("DefaultChatSystemChatEvents").SayMessageRequest:FireServer(message, "All") -- only works on old message system
 end)
 
 CommandModule:Start()
